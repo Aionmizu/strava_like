9a4c568da3647e2c533de8a65ce9623850cd82a8
Aionmizu <marjolinf@gmail.com>
2025-05-05 10:06:29 +0200

    ff

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,17 +1,18 @@
-const mysql = require('mysql2');
-
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
-db.connect(err => {
-    if (err) throw err;
-    console.log("Connected to MySQL");
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+    }
+);
 
-module.exports = db;
+module.exports = sequelize;
+
+

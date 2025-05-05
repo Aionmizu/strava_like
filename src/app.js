7042957be9c45336be51fae92d1123a877ea5642
Aionmizu <marjolinf@gmail.com>
2025-05-05 11:42:18 +0200

    ff

--- src/app.js
+++ src/app.js
@@ -5,6 +5,23 @@ const server = http.createServer(app);
 
 require('dotenv').config();
 
+//Routes
+
+const activityRouter = require('./routes/activity');
+const authRouter = require('./routes/auth');
+const followerRouter = require('./routes/follower');
+const likeRouter = require('./routes/like');
+const statsRouter = require('./routes/stats');
+const userRouter = require('./routes/user')
+
+app.use('/api/v1', activityRouter);
+app.use('/api/v1', authRouter);
+app.use('/api/v1', followerRouter);
+app.use('/api/v1', likeRouter);
+app.use('/api/v1', statsRouter);
+app.use('/api/v1', userRouter);
+
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=>{
     console.log(`Le serveur est actuellement demarrée sur le port $ ${PORT}`);
